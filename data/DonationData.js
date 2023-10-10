61d9890503d13a3cb790487e1e57e084e3a57c10
jabedalamakash <jabedalamakash00@gmail.com>
2023-10-10 09:24:05 +0600

    api update

--- data/DonationData.js
+++ data/DonationData.js
@@ -7,7 +7,7 @@ export const donationData=[
         category:"Health",
         bgColor:"bg-blue-200",
         categoryColor:"bg-blue-300 text-blue-700",
-        titleColor:"text-blue-700",
+        titleColor:"text-blue-700 font-bold",
         btnColor:"bg-blue-700 text-white",
         price:280,
         desc:"All children deserve a healthy start in life. As a global leader in child health, The Sky Foundation keeps children healthy and thriving.We led the massive worldwide effort that helped reduce newborn deaths by over 40% since 2000, saving 1.5 million babies each year. Still, an estimated 5.3 million young children die each year from entirely preventable diseases, such as pneumonia, malaria and diarrhea.As COVID-19 continues to spread globally, health systems around the world are overwhelmed. Children’s access to routine health services and nutritious food is in danger. High levels of hunger and malnutrition have also been exacerbated by the pandemic.We know the world’s children deserve better. As the world’s leading expert on childhood, we have the expertise and the global presence to deliver what’s needed now for children."
@@ -22,7 +22,7 @@ export const donationData=[
         category:"Eduaction",
         bgColor:"bg-red-200 ",
         categoryColor:"bg-red-300 text-red-700",
-        titleColor:"text-red-700",
+        titleColor:"text-red-700 font-bold",
         btnColor:"bg-red-700 text-white",
         price:300,
         desc:"Every child deserves the opportunity to learn.The Sky Foundation works around the world to reach those children who are missing out most on learning and education.We help children get ready for kindergarten and learn to read by third grade — a major indicator of future success. We’re especially focused on reaching vulnerable children in rural America where early learning resources are scarce. Globally, we ensure that no child’s learning stops because they are caught up in crisis. Your donation today supports our work to keep children healthy, educated and safe."
@@ -52,7 +52,7 @@ export const donationData=[
         category:"Food",
         bgColor:"bg-orange-200",
         categoryColor:"bg-orange-300 text-orange-700",
-        titleColor:"text-orange-700",
+        titleColor:"text-orange-700 font-bold",
         btnColor:"bg-orange-700 text-white",
         price:350,
         desc:"153 Million Children Around the World Are Facing Food Insecurit. Hunger drains a child's energy, silences their play and turns their dreams to dust. However, hunger is not a lost cause. Globally, the number of people in famine-like conditions has almost halved. Through The Sky Foundation's work, we've supported 35 million children with hunger-related interventions"
