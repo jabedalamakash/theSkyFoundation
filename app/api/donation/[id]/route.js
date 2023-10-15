ca65017978e60dd83e6f5dfc10bb0fa27a65d778
jabedalamakash <jabedalamakash00@gmail.com>
2023-10-15 10:55:23 +0600

    api dynamic  route created

--- app/api/donation/[id]/route.js
+++ app/api/donation/[id]/route.js
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 export async function GET(req,res) {
     console.log(res.params.id);
 
-    const data=donationData.filter((donation)=> donation.id == req.params.id);
+    const data=donationData.filter((donation)=> donation.id == res.params.id);
 
     if(data.length==0){
         return NextResponse.json({messege:"No Data Found"})
--- app/details/[id]/page.jsx
+++ app/details/[id]/page.jsx
@@ -1,25 +1,27 @@
 
 
 
-// const resData=async(id)=>{
-//     const response= await fetch (`https://theskyfoundation.vercel.app/api/donation/${id}`, { cache: 'no-store' })  
-//     return response.json();
+const resData=async(id)=>{
+    const response= await fetch (`https://theskyfoundation.vercel.app/api/donation/${id}`, { cache: 'no-store' })  
+    return response.json();
 
-// }
+}
 
-// const page = async({params}) => {
-//     // const {id}=params;
+const page = async({params}) => {
+    // const {id}=params;
    
-//     const id=(params.id);
-//     console.log(id);
-//     // const res= await resData(params.id);
-//     // const data=res.donationData;
-//     // console.log(data);
-//     return (
-//         <div>
-            
-//         </div>
-//     );
-// };
+    const id=params.id;
+     console.log(id);
+    const res= await resData(id);
+    const data=res.donationData;
+    console.log(data);
+    return (
+        <div>
 
-// export default page;
+            <h1>Hello</h1>
+            
+        </div>
+    );
+};
+
+export default page;
