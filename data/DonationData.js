8c12d752b9a975e0ab1fc9213368347a0f391e21
jabedalamakash <jabedalamakash00@gmail.com>
2023-10-18 19:49:43 +0600

    api color

--- data/DonationData.js
+++ data/DonationData.js
@@ -5,10 +5,10 @@ export const donationData=[
         img:"/donation/health.png",
         title:"Clean water for children",
         category:"Health",
-        bgColor:" bg-[#D9E5FF]",
-        categoryColor:" bg-[#AEC8FF] text-[#0052FF] px-4 py-2 mb-5",
-        titleColor:"text-[#0052FF] font-bold",
-        btnColor:"bg-[#0052FF]",
+        bgColor:" bg-blue-100",
+        categoryColor:" bg-blue-200 text-blue-600 px-4 py-2 mb-5",
+        titleColor:"text-blue-600 font-bold",
+        btnColor:"bg-blue-600 ",
         price:280,
         desc:"All children deserve a healthy start in life. As a global leader in child health, The Sky Foundation keeps children healthy and thriving.We led the massive worldwide effort that helped reduce newborn deaths by over 40% since 2000, saving 1.5 million babies each year. Still, an estimated 5.3 million young children die each year from entirely preventable diseases, such as pneumonia, malaria and diarrhea.As COVID-19 continues to spread globally, health systems around the world are overwhelmed. Children's access to routine health services and nutritious food is in danger. High levels of hunger and malnutrition have also been exacerbated by the pandemic.We know the world's children deserve better. As the world's leading expert on childhood, we have the expertise and the global presence to deliver what's needed now for children."
 
@@ -20,10 +20,10 @@ export const donationData=[
         img:"/donation/education.png",
         title:"Good Education",
         category:"Eduaction",
-        bgColor:"bg-[#FFE3E4]",
-        categoryColor:"bg-[#FFC3C5] text-[#FF444A]  px-4 py-2 mb-5",
-        titleColor:"text-[#FF444A] font-bold",
-        btnColor:"bg-[#FF444A]",
+        bgColor:"bg-red-100",
+        categoryColor:"bg-red-200 text-red-500 px-4 py-2 mb-5",
+        titleColor:"text-red-500 font-bold",
+        btnColor:"bg-red-500",
         price:300,
         desc:"Every child deserves the opportunity to learn.The Sky Foundation works around the world to reach those children who are missing out most on learning and education.We help children get ready for kindergarten and learn to read by third grade — a major indicator of future success. We're especially focused on reaching vulnerable children in rural America where early learning resources are scarce. Globally, we ensure that no child's learning stops because they are caught up in crisis. Your donation today supports our work to keep children healthy, educated and safe."
 
@@ -35,10 +35,10 @@ export const donationData=[
         img:"/donation/clothing.png",
         title:"Used Clothing Drive",
         category:"Clothing",
-        bgColor:"bg-[#EBF6E2]", 
-        categoryColor:"bg-[#D4ECC1] text-[#79C23F] px-4 py-2 mb-5",
-        titleColor:"text-[#79C23F] font-bold",
-        btnColor:"bg-[#79C23F]",
+        bgColor:"bg-lime-100", 
+        categoryColor:"bg-lime-200 text-lime-500 px-4 py-2 mb-5",
+        titleColor:"text-lime-500 font-bold",
+        btnColor:"bg-lime-500",
         price:250,
         desc:"Whether you're moving, reorganizing the house, have kids that grew out of their clothes or you're just spring cleaning…have you ever considered donating those items to the Purple Heart Foundation? We'll take them! It's fast, free, better for the environment, supports veterans and is tax deductible for you! It's a win-win situation, all the way around! When you donate your new and gently used items to the Purple Heart Foundation, local thrift shops sell them in their stores and donate the revenue back to the Purple Heart Foundation so we may provide valuable services and resources for veterans across the country."
 
@@ -50,10 +50,10 @@ export const donationData=[
         img:"/donation/food.png",
         title:"Better Food for Everyone",
         category:"Food",
-        bgColor:"bg-[#FEEAE3]",
-        categoryColor:"bg-[#FDD2C4] text-[#F87147] px-4 py-2 mb-5",
-        titleColor:"text-[#F87147] font-bold",
-        btnColor:"bg-[#F87147]",
+        bgColor:"bg-orange-100",
+        categoryColor:"bg-orange-200 text-orange-400 px-4 py-2 mb-5",
+        titleColor:"text-orange-400 font-bold",
+        btnColor:"bg-orange-400",
         price:350,
         desc:"153 Million Children Around the World Are Facing Food Insecurit. Hunger drains a child's energy, silences their play and turns their dreams to dust. However, hunger is not a lost cause. Globally, the number of people in famine-like conditions has almost halved. Through The Sky Foundation's work, we've supported 35 million children with hunger-related interventions"
 
