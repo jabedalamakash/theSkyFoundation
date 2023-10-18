6941e028e0d04d30ebad3eac051e238ce88fee3b
jabedalamakash <jabedalamakash00@gmail.com>
2023-10-18 21:05:05 +0600

    details page done

--- app/details/[id]/page.jsx
+++ app/details/[id]/page.jsx
@@ -13,48 +13,47 @@ const page = async({params}) => {
     //  console.log(id);
     const res= await resData(id);
     const data=res.data;
-    console.log(data);
+    // console.log(data);
     return (
         <>
-        <section className='w-full sm:w-[80%] h-screen mx-auto px-5 my-32 sm:px-0'>
+
+        <section className='w-full sm:w-[80%] h-screen mx-auto px-5 my-20 sm:my-32 sm:px-0'>
 
 
         <main>
-            
-            <div className=" relative  py-20">
+          
+            <div className=" relative  mb-20">
                 <Image
                 src={data[0].img}
                 width={500}
                 height={500}
                 alt={data[0].title}
                 priority
-                className=" w-full h-[700px] absolute mb-20 "
-                />
+                className=" w-full relative h-[400px] sm:h-[700px] rounded-b-xl  -mb-20"/>
 
-            <div className="relative bg-black/30 p-5 sm:p-10 bottom-0  left-0  w-full  rounded-b-xl ">
-             
-                <button className={`${data[0].btnColor} rounded-lg font-bold text-white  px-5 py-2`}>Donate ${data[0].price}</button>
-
-                
-                </div>
-
-
-
-
+            <div className="absolute bg-black/30 p-5 sm:p-10 
+            bottom-0 rounded-b-xl  left-0  w-full z-10 ">
+                <button className={`${data[0].btnColor} rounded-lg
+                font-bold text-white  px-5 py-2`}>Donate ${data[0].price}</button>      
             </div>
+            </div>
+           
+
+
+
             </main>
-
-
-
-            <article className="w-full z-[3] h-screen mt-32">
+      
+            <article className="w-full py-20 sm:py-30 h-auto">
             <h1 className="text-3xl mb-10  tracking-wide font-bold ">{data[0]?.title}</h1>
             <p className="leading-loose">{data[0]?.desc}</p>
             </article>
 
-
             </section>
+
+            </>
+
+
             
-        </>
     );
 };
 
