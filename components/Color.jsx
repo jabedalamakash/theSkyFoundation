348dfa5c1f5fac92c87bc368032d3eccbc4024c1
jabedalamakash <jabedalamakash00@gmail.com>
2023-11-14 00:18:59 +0600

    colors

--- components/Color.jsx
+++ components/Color.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Color = () => {
     return (
       <div>
-        <p className="bg-[#D9E5FF] hover:bg-blue-600 text-blue-600"></p> 
-        <span className='bg-[#AEC8FF]'></span>
-        <span className='bg-[#FEEAE3]'></span>
-        <span className='bg-[#FFE3E4]'></span>
-        <span className='bg-lime-100'></span>
+        <p className="bg-[#D9E5FF] hover:bg-blue-600 text-blue-600">i</p> 
+        <span className='bg-[#AEC8FF]'>he</span>
+        <span className='bg-[#FEEAE3]'>hi</span>
+        <span className='bg-[#FFE3E4]'>ho</span>
+        <span className='bg-lime-100'>hello</span>
 
-        <p className=" hover:bg-[#FDD2C4] text-[#F87147] bg-[#F87147]  "></p>
-        <p className=" hover:bg-[#FFC3C5] text-[#FF444A] bg-[#FF444A]"></p>
-        <p className=" hover:bg-lime-200 text-lime-500 bg-lime-500 "></p>
+        <p className=" hover:bg-[#FDD2C4] text-[#F87147] bg-[#F87147]  ">am</p>
+        <p className=" hover:bg-[#FFC3C5] text-[#FF444A] bg-[#FF444A]">ak</p>
+        <p className=" hover:bg-lime-200 text-lime-500 bg-lime-500 ">ash</p>
       </div>
     );
 };
